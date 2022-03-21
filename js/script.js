bf1d37894d1daf74bbf7d17ff09c59541306f87d
Roman <ramenformom@gmail.com>
2022-03-21 15:21:01 +0300

    implemented sending data to the server

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-    var tab = document.querySelectorAll('.info-header-tab'),
+    let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
-        for (var i = a; i < tabContent.length; i++) {
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
@@ -20,9 +20,9 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
     info.addEventListener('click', function (e) {
-        var target = e.target;
+        let target = e.target;
         if (target && target.classList.contains('info-header-tab')) {
-            for (var i = 0; i < tab.length; i++) {
+            for (let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
@@ -33,60 +33,121 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    var deadLine = '2022-04-01';
+    let deadLine = '2022-04-01';
 
-    function getTimeRemaining(endTime){
-        var t = Date.parse(endTime) - Date.parse(new Date());
-        if(t > 0){
-        var seconds = Math.floor((t/1000)%60);
-        var minutes = Math.floor((t/1000/60)%60);
-        var hours = Math.floor((t/(1000*60*60)));
-        return{
-            'total' : t,
-            'hours' : hours,
-            'minutes' : minutes,
-            'seconds' : seconds
-        };
-        }else {
-            return{
-                'total' : 0,
-                'hours' : 0,
-                'minutes' : 0,
-                'seconds' : 0
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        if (t > 0) {
+            let seconds = Math.floor((t / 1000) % 60),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                hours = Math.floor((t / (1000 * 60 * 60)));
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        } else {
+            return {
+                'total': 0,
+                'hours': 0,
+                'minutes': 0,
+                'seconds': 0
             };
         }
     }
 
-    function setCloack(id, endtime){
-        var timer = document.querySelector('.timer');
-        var hourse = timer.querySelector('.hours');
-        var minutes = timer.querySelector('.minutes');
-        var seconds = timer.querySelector('.seconds');
-        var timeInterval = setInterval(updateClock, 1000);
+    function setCloack(id, endtime) {
+        let timer = document.querySelector('.timer'),
+            hourse = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
         function updateClock() {
-            var t = getTimeRemaining(endtime);
-            if (t.hours <= 9){
-            hourse.textContent = "0" + t.hours  ;
-            } else{
+            let t = getTimeRemaining(endtime);
+            if (t.hours <= 9) {
+                hourse.textContent = "0" + t.hours;
+            } else {
                 hourse.textContent = t.hours;
             }
-            if (t.minutes <= 9 ){
+            if (t.minutes <= 9) {
                 minutes.textContent = "0" + t.minutes;
-            }else {
+            } else {
                 minutes.textContent = t.minutes;
             }
-            if (t.seconds <= 9){
-            seconds.textContent = "0" + t.seconds;
-            } else{
+            if (t.seconds <= 9) {
+                seconds.textContent = "0" + t.seconds;
+            } else {
                 seconds.textContent = t.seconds;
             }
 
-            if( t.total <= 0){
+            if (t.total <= 0) {
                 clearInterval(timeInterval);
             }
         }
     }
 
     setCloack('timer', deadLine);
+
+
+
+    let wind = document.querySelector(".overlay"),
+        moreBtn = document.querySelector(".more"),
+        close = document.querySelector('.popup-close');
+
+    moreBtn.addEventListener('click', function () {
+        wind.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hiden';
+    });
+
+    close.addEventListener('click', function () {
+        wind.style.display = 'none';
+        moreBtn.classList.remove('more-splash');
+    });
+
+    let massege = {
+        loading: "Загрузка...",
+        success: "Спасибо, скоро мы с вами свяжемся!",
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName("input"),
+        statusMassage = document.createElement("div");
+    statusMassage.classList.add('status');
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        form.appendChild(statusMassage);
+
+        let requst = new XMLHttpRequest();
+        requst.open('POST', 'server.php');
+        requst.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key){
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+                requst.send(json);
+
+        requst.addEventListener('readystatechange', function () {
+            if (requst.readyState < 4) {
+                statusMassage.innerHTML = massege.loading;
+            } else if (requst.readyState === 4 && requst.status == 200) {
+                statusMassage.innerHTML = massege.success;
+            } else {
+                statusMassage.innerHTML = massege.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++){
+            input[i].value = "";
+        }
+
+    });
 });
